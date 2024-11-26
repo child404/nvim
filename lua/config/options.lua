4d8363eb6d2be2e7fa5af0bb69d2c1b5cbbe5a0e
child404 <paperphoenixx@pm.me>
2024-11-26 17:25:54 +0200

    refactor(options): formatting

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,46 +1,47 @@
 local opt = vim.opt
 
 local options = {
-    guicursor = "",
+	guicursor = "",
 
-    number = true, -- show line numbers
-    relativenumber = true, -- show relative line numbers
+	number = true, -- show line numbers
+	relativenumber = true, -- show relative line numbers
 
-    tabstop = 4, -- 4 spaces for a tab
-    softtabstop = 4, -- same as the next ???
-    shiftwidth = 4, -- num of spaces for each indent
-    expandtab = true, -- tabs to spaces
+	tabstop = 4, -- 4 spaces for a tab
+	softtabstop = 4, -- same as the next ???
+	shiftwidth = 4, -- num of spaces for each indent
+	expandtab = true, -- tabs to spaces
 
-    smartindent = true, -- make indenting smarter again
+	smartindent = true, -- make indenting smarter again
 
-    wrap = false, -- display lines as one long line
+	wrap = false, -- display lines as one long line
 
-    swapfile = false, -- no swap file
-    backup = false, -- no backup
-    undodir = os.getenv("HOME") .. "/.vim/undodir",
-    undofile = true,
+	swapfile = false, -- no swap file
+	backup = false, -- no backup
+	undodir = os.getenv("HOME") .. "/.vim/undodir",
+	undofile = true,
 
-    hlsearch = false, -- highlight matched text
-    incsearch = true, -- increment search
-    ignorecase = true, -- ignore case in search
-    smartcase = true, -- ingore previous if uppercase in search
+	hlsearch = false, -- highlight matched text
+	incsearch = true, -- increment search
+	ignorecase = true, -- ignore case in search
+	smartcase = true, -- ingore previous if uppercase in search
+	inccommand = "split",
 
-    termguicolors = true, -- enable terminal colors
+	termguicolors = true, -- enable terminal colors
 
-    scrolloff = 8, -- when to stop scrolling (lines before the bottom)
-    sidescrolloff = 8,
-    signcolumn = "yes", -- column on the left side
-    
-    updatetime = 50,
+	scrolloff = 8, -- when to stop scrolling (lines before the bottom)
+	sidescrolloff = 8,
+	signcolumn = "yes", -- column on the left side
 
-    splitright = true, -- vsplit window opens at the right
-    splitbelow = true, -- hsplit window opens at the bottom
+	updatetime = 50,
 
-    mouse = "a", -- enable mouse everywhere
+	splitright = true, -- vsplit window opens at the right
+	splitbelow = true, -- hsplit window opens at the bottom
+
+	mouse = "a", -- enable mouse everywhere
 }
 
 opt.isfname:append("@-@")
 
 for k, v in pairs(options) do
-    opt[k] = v
+	opt[k] = v
 end
