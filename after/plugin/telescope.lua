2975929df14fc12bc929728287a76d0817fe5ba7
child404 <paperphoenixx@pm.me>
2024-11-28 15:22:40 +0200

    feat(telescope): add fzf and ui-select; add multi-ripgrep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,9 +1,24 @@
+require("telescope").setup({
+	extensions = {
+		wrap_results = true,
+
+		fzf = {},
+		["ui-select"] = {
+			require("telescope.themes").get_dropdown({}),
+		},
+	},
+})
+
+pcall(require("telescope").load_extension, "fzf")
+pcall(require("telescope").load_extension, "ui-select")
+
 local map = vim.keymap.set
 local builtin = require("telescope.builtin")
 
 map("n", "<leader>fd", builtin.find_files, { desc = "Telescope find files" })
 map("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 map("n", "<leader>fs", builtin.live_grep, { desc = "Telescope live grep" })
+map("n", "<leader>fS", require "config.telescope.multi-ripgrep")
 map("n", "<leader>fg", function()
 	builtin.grep_string({ search = vim.fn.input("Grep: ") })
 end, { desc = "Telescope grep string" })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,9 @@
 return {
-	"nvim-telescope/telescope.nvim", branch = "0.1.x",
-	dependencies = { "nvim-lua/plenary.nvim" }
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-telescope/telescope-ui-select.nvim",
+	},
 }
