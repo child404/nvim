009ccf3802a86665ed24b4263aea2a5688387f81
child404 <paperphoenixx@pm.me>
2024-11-28 15:00:25 +0200

    feat(lsp): add cmp signature help

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,6 +8,7 @@ return {
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-cmdline",
 		"hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-nvim-lsp-signature-help",
 		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip",
 		"j-hui/fidget.nvim",
@@ -93,6 +94,7 @@ return {
 			}, {
 				{ name = "buffer" },
 				{ name = "path" },
+                { name = 'nvim_lsp_signature_help' },
 			}),
 		})
 
