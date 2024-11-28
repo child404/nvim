559d60e5cdf87a28ca5d21b3c30cb467bdd3285d
child404 <paperphoenixx@pm.me>
2024-11-28 12:24:35 +0200

    feat(statusline): add more info; restructure elements

--- lua/config/statusline.lua
+++ lua/config/statusline.lua
@@ -1,3 +1,16 @@
+function MyMode()
+  local modes = {
+    n = "NORMAL",
+    i = "INSERT",
+    v = "VISUAL",
+    V = "V-LINE",
+    c = "COMMAND",
+    t = "TERMINAL",
+    s = "SELECT",
+  }
+  return modes[vim.api.nvim_get_mode().mode] or "UNKNOWN"
+end
+
 function GitStatus()
   return vim.b.gitsigns_status or ""
 end
@@ -6,4 +19,4 @@ function GitBranch()
   return vim.b.gitsigns_head or ""
 end
 
-vim.o.statusline = "%f %m %r %=%{v:lua.GitBranch()} %=%{v:lua.GitStatus()} %y %p%%"
+vim.o.statusline = "[%{v:lua.MyMode()}] <%{v:lua.GitBranch()}> %= %f %m %r %= %{v:lua.GitStatus()} %y[%l:%c]"
